77717f11d538daa433da171b8e7f2b51cedf8201
H. Watanabe <kaityo@users.sourceforge.jp>
2022-01-05 23:55:24 +0900

    adds revolving

--- 01_basic/revolving.py
+++ 01_basic/revolving.py
@@ -1,14 +1,18 @@
+
+
+def num_payment(balance, y_rate, payment):
+    y_rate = 0.15  # Yearly rate
+    i = 0
+    while balance > 0:
+        i = i + 1
+        interest = int(balance * m_rate)
+        pay = payment - interest
+        if balance < pay:
+            pay = balance
+        balance -= pay
+        print(f"{i} {interest} {pay} {interest + pay} {balance}")
+
 balance = 100000
 y_rate = 0.15  # Yearly rate
-m_rate = y_rate / 12.0  # Monthly rate
 payment = 5000  # Monthly payment
-
-i = 0
-while balance > 0:
-    i = i + 1
-    interest = int(balance * m_rate)
-    pay = payment - interest
-    if balance < pay:
-        pay = balance
-    balance -= pay
-    print(f"{i} {interest} {pay} {interest + pay} {balance}")
+num_payment(balance, y_rate, payment)
